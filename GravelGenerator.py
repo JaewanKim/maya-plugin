797f419cf0ed5d6c6fa0bb872ccf6a75c22db21f
JaewanKim <wodhks6561@gmail.com>
2018-04-13 01:35:07 +0900

    Confirm and Add get_values funtion

--- GravelGenerator.py
+++ GravelGenerator.py
@@ -35,8 +35,17 @@ class GravelGenerator():
         
         cmds.columnLayout(adjustableColumn=True)
         cmds.rowLayout(numberOfColumns=3)
+
+        # DirectionControl = cmds.radioCollection()
+        # Direction0 = cmds.radioButton(label='Front')
+        # Direction1 = cmds.radioButton(label='Back')
+        # Direction2 = cmds.radioButton(label='Random')
+        # DirectionControl = cmds.radioCollection( DirectionControl, edit=True, select=Direction0 )
         
-        cmds.radioButtonGrp(label="Generate Type", labelArray2=['File', 'Script'], numberOfRadioButtons=2)
+#        self.radio_col = cmds.radioCollection()
+#        cmds.radioButtonGrp(label="Generate Type", labelArray2=['File', 'Script'], numberOfRadioButtons=2)
+#        self.radio_col = cmds.radioCollection(self.radio_col, edit=True, select='Script')
+
         cmds.setParent("..")
         
         self.amount_str = cmds.intSliderGrp(l="Amount", min=1, max=100, field=True)
@@ -47,12 +56,35 @@ class GravelGenerator():
         cmds.columnLayout(adjustableColumn=True)
         
         cmds.setParent("..")
-        cmds.button(label="Generate Gravels", command=self.recognize_map)
+        cmds.button(label="Generate Gravels", command=self.get_values)
         cmds.button(label="Reset", command=self.reset)
         
         cmds.setParent("..")
         cmds.showWindow(self.win)
+
+    def get_values(self, args):
+        # Get value of Generate type
+        self.amount = cmds.intSliderGrp(self.amount_str, query=True, value=True)
+        self.global_size = cmds.intSliderGrp(self.global_size_str, query=True, value=True)
+        self.max_size = floatSliderGrp(self.max_size_str, query=True, value=True)
+        self.min_size = floatSliderGrp(self.min_size_str, query=True, value=True)
         
+        # self.radioCol = cmds.radioCollection(DirectionControl, query=True, sl=True)
+        # getSelectRadioVal = cmds.radioButton(self.radioCol, query=True, select=True)
+        
+#        self.radio_collect = cmds.radioCollection(self.radio_col, query=True, select=True)
+#        getRadioVal = cmds.radioButton(self.radio_collect, query=True, select=True)
+
+        print(self.amount + self.global_size)
+
+    def make_gravel(self, args):
+        # Generate gravels by script
+        pass
+
+    def get_gravel_file(self, args):
+        # Get the files of gravel, size it randomly
+        # I/O File obj, poly, 
+        pass
         
     def recognize_map(self, args):
         # Recognize the selected object as a map
@@ -75,10 +107,6 @@ class GravelGenerator():
         # Get vertex of the map
         pass
                 
-    def get_gravel_file(self, args):
-        # Get the files of gravel, size it randomly
-        # I/O File obj, poly, 
-        pass
         
     def reset(self, args):
         # Reset the values set by a user
