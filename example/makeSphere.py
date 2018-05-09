3defe0c9fde519c6cad678adba0b8803ab391413
JaewanKim <wodhks6561@gmail.com>
2018-05-09 16:25:34 +0900

    Delete example directory (same as study dir)
    Had a mistake in the merge process

--- example/makeSphere.py
+++
@@ -1,6 +0,0 @@
-# listing of pythonFromMel.py
-
-import maya.cmds as cmds
-
-def makeSphere():
-    cmds.sphere()
--- example/runMEL.py
+++
@@ -1,9 +0,0 @@
-import maya.cmds as cmds
-import maya.mel as mel
-
-def runMEL():
-	print("Running MEL from Python")
-	mel.eval("source myMELScript;")
-	mel.eval("myMELScript;")
-
-runMEL()
